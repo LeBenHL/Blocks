0115ea8abad8c8ec6412cfd1d2de4a930d630290
Benjamin Le <kantas92@berkeley.edu>
2013-02-14 13:34:27 -0800

    Initial Commit

---
+++ rails/app/Blocks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocks::Application.config.secret_token = 'a01f4e01ff806d60303fa63009c3f8cda6af81a9324b1924a0656102c76972166789e2ce67a5208b7423bc6fb1d2e761ee039e6a4d20c0f8a6160197c655a0cc'
